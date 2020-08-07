e67a0d8781739dcfca715643c7fb9cb5e55c74d6
Arjun Chamyal <arjun.chamyal@sofocle.com>
2020-08-07 14:10:58 +0530

    this is the first commit

--- marlin-init.sh
+++ marlin-init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ./beacon/server &
-./relay/masterexec "127.0.0.1:8002" &
-./relay/relayexec "127.0.0.1:8002" &
+#./relay/masterexec "127.0.0.1:8002" &
+#./relay/relayexec "127.0.0.1:8002" &
