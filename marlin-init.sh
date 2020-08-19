060d7794385220169815a9f2226554aa748e8b37
Arjun Chamyal <arjun.chamyal@sofocle.com>
2020-08-19 18:34:07 +0530

    configurable now

--- marlin-init.sh
+++ marlin-init.sh
@@ -2,10 +2,10 @@
 ./beacon/server &
 if [ "$1" = "masterNode" ]
 then
-./relay/masterexec "$2:$3" &
+#./relay/masterexec "$2:$3" &
 echo "masterNode is ready"
 elif [ "$1" = "relayNode" ]
 then
-./relay/relayexec "$2:$3" &
+#./relay/relayexec "$2:$3" &
 echo "relayNode is ready"
 fi
